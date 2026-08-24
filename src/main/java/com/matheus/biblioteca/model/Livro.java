56de3c8a033d7744377122cbb5c26dca1e4e0670
Matheus <Matheusbaia210@gmail.com>
2026-08-24 15:18:49 -0300

    adiciona campo id em livro para suportar persistencia MySQL

--- src/main/java/com/matheus/biblioteca/model/Livro.java
+++ src/main/java/com/matheus/biblioteca/model/Livro.java
@@ -1,6 +1,7 @@
 package com.matheus.biblioteca.model;
 
 public class Livro {
+    private Integer id;
     private String titulo;
     private String autor;
     private String isbn;
@@ -11,6 +12,12 @@ public class Livro {
         this.autor = autor;
         this.isbn = isbn;
         this.disponivel = true;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId (Integer id) {
+        this.id = id;
     }
     @Override
     public String toString() {
